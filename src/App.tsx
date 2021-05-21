99515807501cef9dec88e4626dbe5bfe5eb1b7d4
chul0721 <chul070721@naver.com>
2021-05-22 01:25:26 +0900

    perf: removed not_used things

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
 import React from 'react'
-import $ from 'jquery'
+import Header from './components/Header'
 
 class App extends React.Component {
-  public typingBool: any
-  public typingIdx: any
-  public tyInt: any
-  public typingTxt: any
-
-  typing() {
-    this.typingTxt = $('.typing-txt').text()
-    this.typingTxt = this.typingTxt.split('')
-    this.typingIdx = 0
-    this.typingBool = false
-
-    if (this.typingBool == false) {
-      this.typingBool = true
-      this.tyInt = setInterval(this.typing, 100)
-    }
-
-    if (this.typingIdx < this.typingTxt.length) {
-      $('.typing').append(this.typingTxt[this.typingIdx])
-      this.typingIdx++
-    } else {
-      clearInterval(this.tyInt)
-    }
-  }
-
   render() {
     return (
       <>
-        <div
-          className="text-4xl text-white font-inconsolata"
-          onMouseEnter={this.typing()}
-        >
-          <p className="typing-txt hidden">타이핑될 텍스트 입니다.</p>
-          <p className="typing inline-block"></p>
+        <div className="flex justify-center flex-col">
+          <div className="typewriter linear-wipe text-6xl ml-20 font-dohyeon">
+            김병철
+          </div>
+          <div className="text-white text-xl font-dohyeon flex justify-self-center">
+            지극히 평범한 <div className="typewriter">코딩하는</div> 중학생
+          </div>
         </div>
       </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inconsolata: ['Inconsolata']
+        nanum: ['NanumGothic'],
+        blackhansans: ['BlackHanSans'],
+        dohyeon: ['DoHyeon']
       }
     }
   },
